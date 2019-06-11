849ff0bd21c8a11e2fb55a3ca5378a8d872cbcc2
Dipto Chakrabarty <diptochuck123@gmail.com>
2019-06-11 15:14:01 +0530

    playbooks

--- Dipto/sock_menu.py
+++ Dipto/sock_menu.py
@@ -9,22 +9,52 @@ cl,addr=s.accept()
 while True:
 	data=cl.recv(50)
 	cmd=data.decode()
-	cl.send(data)
 	if cmd=="adduser":
 		data2=cl.recv(20)
 		username=data2.decode()
 		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/add_user.yml --extra-vars 'var1={}'".format(username))
 	if cmd=="dockerlaunch":
+		data0=cl.recv(20)
+		ip=data0.decode()
 		data2=cl.recv(20)
 		name=data2.decode()
-		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/Dockers/docker_update.yml --extra-vars  'var1={}'".format(name))
+		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/Dockers/docker_update.yml --extra-vars 'var0={}' --extra-vars  'var1={}'".format(ip,name))
 	if cmd=="httpd":
-		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/Web/httpd_launch.yml")
+		data0=cl.recv(20)
+		ip=data0.decode()
+		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/Web/httpd_launch.yml--extra-vars  'var0={}'".format(ip))
 	if cmd=="nginx":
-		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/Web/nginx.yml")
+		data0=cl.recv(20)
+		ip=data0.decode()
+		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/Web/nginx.yml --extra-vars  'var0={}'".format(ip))
 	if cmd=="mail":
-		sp.getoutput("ansible-playbook /root/Desktop/root/Desktop/Github/Menu-Automation/Mail/mail_send.yml")
- 
+		data0=cl.recv(50)
+		ml=data0.decode()
+		data1=cl.recv(20)
+		at=data1.decode()	
+		sp.getoutput("ansible-playbook /root/Desktop/root/Desktop/Github/Menu-Automation/Mail/mail_send.yml --extra-vars  'var0={}' --extra-vars 'var_attach={}'".format(ml,at))
+	if cmd=="date":
+		data0=cl.recv(20)
+		ip=data0.decode()
+		dt= sp.getoutput("date")
+		cl.send(dt.encode())
+	if cmd=="remoteadduser":
+		data0=cl.recv(20)
+		ip=data0.decode()
+		data2=cl.recv(20)
+		username=data2.decode()
+		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/remote.yml --extra-vars  'var0={}' --extra-vars 'var1={}'".format(ip,username))
+	if cmd=="sassfire":
+		data0=cl.recv(20)
+		host=data0.decode()
+		data1=cl.recv(20)
+		name=data1.decode()
+		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/Dockers/docker.yml --extra-vars 'var0={}' --extra-vars 'var1={}'".format(host,name))
+		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/Dockers/saas.yml  --extra-vars  'var0={}'".format(host)) 
+	if cmd=="yum":
+		data0=cl.recv(20)
+		host=data0.decode()
+		sp.getoutput("ansible-playbook /root/Desktop/Github/Menu-Automation/systems/repolist.yml --extra-vars  'var0={}'".format(host))
 	if cmd=="exit":
 		break
 
